6a3ea754fe5fb35074c91e6857cd6e47542a5248
roleyder <roleyder02@gmail.com>
2020-04-10 22:52:47 -0400

    test template

---
+++ template.js
@@ -0,0 +1,15 @@
+export default () => {
+    return `
+            <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Project management</title>
+        </head>
+        <body>
+            <h1>Test</h1>
+        </body>
+        </html>
+    `;
+}
