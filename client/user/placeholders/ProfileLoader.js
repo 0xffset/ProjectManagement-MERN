085819dd8e5aec4765ed210ffdb64d3feb448426
roleyder <roleyder02@gmail.com>
2020-04-23 12:48:02 -0400

    Placeholder alignment fixed

--- client/user/placeholders/ProfileLoader.js
+++ client/user/placeholders/ProfileLoader.js
@@ -17,7 +17,7 @@ const ProfileLoader = () => (
     
     <rect x="6" y="368" rx="2" ry="2" width="140" height="10" /> 
     <rect x="6" y="341" rx="2" ry="2" width="204" height="15" /> 
-    <rect x="6" y="22" rx="2" ry="2" width="304" height="304" /> 
+    <rect x="6" y="22" rx="2" ry="2" width="275" height="275" /> 
     <rect x="6" y="392" rx="0" ry="0" width="144" height="13" /> 
     <rect x="6" y="433" rx="0" ry="0" width="152" height="12" />
   </ContentLoader>
