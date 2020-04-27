684e802a689f005682ab29cd740911ab175fba69
roleyder <roleyder02@gmail.com>
2020-04-27 09:46:00 -0400

    fetch all repositories completed

--- client/core/dashboard.js
+++ client/core/dashboard.js
@@ -1,63 +1,81 @@
-import React, {Component} from 'react';
-import {Segment, Responsive, Button, Icon, Table, Container} from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Segment, Responsive, Button, Icon, Table, Container } from 'semantic-ui-react';
 import MenuUI from './Menu';
-
+import {isAuthenticated} from '../auth/auth-helper';
+import axios from 'axios';
 
 //__test__
-import {Projects} from '../__test__/projects.json.json'
+import { Projects } from '../__test__/projects.json.json'
 export default class Dashboard extends Component {
-    constructor() {
-        super()
-        this.state =({
-          open: true
-        })
-      }
-      
-    
-render() {
-      
-        return (
-<div>
-<MenuUI />
+  constructor() {
+    super()
+    this.state = ({
+      open: true,
+      repos: []
+    })
+   
+}
 
+getLanguageIcon(language) {
 
-<Container className='containerDashboard' style={{marginTop : '30px'}} >
-<Table celled compact definition>
-    <Table.Header fullWidth>
-      <Table.Row>
-        <Table.HeaderCell />
-        <Table.HeaderCell>Name</Table.HeaderCell>
-        <Table.HeaderCell>Description</Table.HeaderCell>
-        <Table.HeaderCell>Languaje</Table.HeaderCell>
-        <Table.HeaderCell>GitHub repository</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+let lan = {
+  "JavaScript" : 'js',
+  "PHP": 'php',
+  "Python": 'python',
+}
+  return lan[language];
+}
 
-    <Table.Body>
-      {Projects.map(project => 
-         <Table.Row  key={project._id}> 
-         <Table.Cell collapsing>
-         <Button size='small'>Detail</Button>
-         </Table.Cell>
-         <Table.Cell>{project.Name}</Table.Cell>
-         <Table.Cell>{project.Description}</Table.Cell>
-         <Table.Cell>{project.Language}</Table.Cell>
-         <Table.Cell><a href={project.GitHubRepo} target='_blank'>{project.GitHubRepo}</a></Table.Cell>
-       </Table.Row>
-        )}
-     
-    </Table.Body>
-     <Table.Footer fullWidth>
-      <Table.Row>
-        <Table.HeaderCell />
-        <Table.HeaderCell colSpan='4'>
-       
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Footer>
-  </Table>
-</Container>
-  </div>
-        )
-    }
+componentDidMount() {
+      axios.get(`https://api.github.com/users/${isAuthenticated().user.githubname}/repos`)
+        .then((repos) => {
+          this.setState({repos: repos.data})
+      })
+      console.log(this.state.repos)
+}
+
+  render() {
+
+    return (
+      <div>
+        <MenuUI />
+        <Container className='containerDashboard' style={{ marginTop: '30px' }} >
+          <Table celled compact definition>
+            <Table.Header fullWidth>
+              <Table.Row>
+                <Table.HeaderCell />
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Description</Table.HeaderCell>
+                <Table.HeaderCell>Languaje</Table.HeaderCell>
+                <Table.HeaderCell>GitHub repository</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {this.state.repos.map(repos =>
+                <Table.Row key={repos.id}>
+                  <Table.Cell collapsing>
+                    <Button size='small'>Detail</Button>
+                  </Table.Cell>
+              <Table.Cell>{repos.name} {repos.fork ? (<Icon name='fork'/>) : (<></>) }</Table.Cell>
+                  <Table.Cell>{repos.description}</Table.Cell>
+              <Table.Cell>{repos.language}<Icon name={`${this.getLanguageIcon(repos.language)}`}/></Table.Cell>
+                  <Table.Cell><a href={repos.html_url} target='_blank'>{repos.html_url}</a></Table.Cell>
+                </Table.Row>
+              )}
+
+            </Table.Body>
+            <Table.Footer fullWidth>
+              <Table.Row>
+                <Table.HeaderCell />
+                <Table.HeaderCell colSpan='4'>
+
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
+        </Container>
+      </div>
+    )
+  }
 }
