455e6f84afb3f297b7d71fe7e0b9edcbc8e5e5ff
roleyder <roleyder02@gmail.com>
2020-06-12 12:26:29 -0400

    production

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 const config = {
-    env: process.env.NODE_ENV || 'development',
+    env: process.env.NODE_ENV || 'production',
     port: process.env.PORT || 3000,
     jwSecret: process.env.JW_SECRET || "12s1dsqrsprsqzxs2-secret",
-    mongoUri: process.env.MONGODB_URL || process.env.MONGODB_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGODB_PORT || '27017') +
-    '/productManagement'
+    mongoUri: 'mongodb://uyypaup5leuiffbniqxm:SC2KzSKT2dnWyzrXLO3r@bzsbc1jacqfyloz-mongodb.services.clever-cloud.com:27017/bzsbc1jacqfyloz'
+    // mongoUri: process.env.MONGODB_URL || process.env.MONGODB_HOST ||
+    // 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGODB_PORT || '27017') +
+    // '/productManagement'
 }
 
 export default config;
