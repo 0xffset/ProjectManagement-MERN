b3098c5c45573c8896c0a9d1c201e5aaaa8d6f05
roleyder <roleyder02@gmail.com>
2020-06-12 11:57:00 -0400

    release v1.0.0

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -4,10 +4,6 @@ const authenticate = (jwt, ch) => {
     if(typeof window !== 'undefined') {
         sessionStorage.setItem('jwt', JSON.stringify(jwt));
         ch()
-<<<<<<< HEAD
-=======
-        
->>>>>>> 34a6a28b72cff1acef8130d886fd3d83f496a4a7
     }
 }
 
